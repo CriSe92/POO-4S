7bc52e7eaa6261dd692d87776efe5d39c07ba937
CriSe92 <cristina92.serrao@gmail.com>
2023-03-27 15:35:45 +0200

    Add files via upload

---
+++ src/esempi/StackInt.java
@@ -0,0 +1,22 @@
+package esempi;
+
+public class StackInt {
+	private int[] pila;
+	private int p = 0;
+	
+	public StackInt() {}
+	
+	public int pop() {} //rimozione
+	
+	public void push() {} //inserimento
+	
+	public boolean pieno() {}
+	
+	public boolean vuoto() {}
+	
+	public int size() {} //quanti elementi ci sono nello stack
+	
+	public static void main(String[] args) {
+		//Prova lo Stack
+	}
+}
