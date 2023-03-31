bcaefcd3f2e7847230ca8c5b27ffce6b26d80fd5
CriSe92 <cristina92.serrao@gmail.com>
2023-03-31 15:47:05 +0200

    Update StackInt.java

--- src/esempi/StackInt.java
+++ src/esempi/StackInt.java
@@ -4,19 +4,71 @@ public class StackInt {
 	private int[] pila;
 	private int p = 0;
 	
-	public StackInt() {}
+	public StackInt(int n) {
+		pila = new int[n];
+	}
 	
-	public int pop() {} //rimozione
+	public int pop() {
+		if(p>0) {
+			int x = pila[p-1];
+			p--;
+			return x;
+		}
+		System.out.println("Pila vuota");
+		return -1;
+	} //rimozione
 	
-	public void push() {} //inserimento
+	public void push(int x) {
+		if(p<pila.length) {
+			pila[p] = x;
+			p++;
+		}else {
+			System.out.println("Stack Pieno!");
+		}
+	} //inserimento
 	
-	public boolean pieno() {}
+	public boolean pieno() {
+		return p == pila.length;
+	}
 	
-	public boolean vuoto() {}
+	public boolean vuoto() {
+		/*if(p == 0) {
+			return true;
+		}else {
+			return false;
+		}*/
+		
+		return p == 0;
+	}
 	
-	public int size() {} //quanti elementi ci sono nello stack
+	public int size() { //quanti elementi ci sono nello stack
+		return p;
+	} 
+	
+	public void stampaStack() {
+		for(int i=0; i<p; i++) {
+			System.out.println(pila[i]);
+		}
+	}
 	
 	public static void main(String[] args) {
 		//Prova lo Stack
+		StackInt s = new StackInt(5);
+		
+		s.push(52);
+		s.push(20);
+		s.push(0);
+		s.push(12);
+		s.push(15);
+		
+		s.push(18);
+		
+		int x = s.pop();
+		System.out.println("Valore restituito: "+x);
+		x = s.pop();
+		System.out.println("Valore restituito: "+x);
+		s.push(30);
+		s.stampaStack();
+		
 	}
 }
